634979ff2d8aeef1f3956920b504bf369ddc8d94
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-05-17 20:53:41 +0530

    style:update style of options

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,46 +1,56 @@
-import {plane,wine,house,coins} from 'lucide-react';
+import { 
+  UserRound, 
+  UserPlus, 
+  Users, 
+  Wallet, 
+  CreditCard, 
+  Diamond, 
+  PlaneTakeoff
+} from 'lucide-react';
 
-
-export const SelectTravelesList=[
+export const SelectTravelesList = [
     {
-        id:1,
-        title:'Just Me',
-        desc:'A sole Traveles in explorartion',
-        icon: plane,
-        people:'1'
-
+        id: 1,
+        title: 'Just Me',
+        desc: 'A sole Traveler in exploration',
+        icon: UserRound,
+        people: '1'
     },
     {
-        id:2,
-        title:'A Couple',
-        desc:'Two Traveles in tandem',
-        icon: wine,
-        people:'2'
+        id: 2,
+        title: 'A Couple',
+        desc: 'Two Travelers in tandem',
+        icon: UserPlus,
+        people: '2'
     },
     {
-        id:3,
-        title:'Family',
-        desc:'A group of fun loving adv',
-        icon: house,
-        people:'3 to 5 people',
-
+        id: 3,
+        title: 'Family',
+        desc: 'A group of fun loving adventurers',
+        icon: Users,
+        people: '3 to 5 people',
     },
-]
+];
 
-export const SelectBudgetOptions=[
+export const SelectBudgetOptions = [
     {
-        id:1,
-        title:'Cheap',
-        desc:'Stay Conscious of costs',
-        icon: coins,
-        
+        id: 1,
+        title: 'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: Wallet,
     },
     {
-        id:2,
-        title:'Moderate',
-        desc:'Keep cost on the average side',
-        icon: wine,
-    }
+        id: 2,
+        title: 'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: CreditCard,
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'Don\'t worry about the cost',
+        icon: Diamond,
+    },
+];
 
-]
-
+export const AI_PROMPT = 'Generate Travel Plan for Location: {location}';
